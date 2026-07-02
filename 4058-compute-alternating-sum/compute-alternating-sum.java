a2540973cd105aa61afba49260d22a4a51581b75
Mohanapriya1222 <mohanapriyak.cse2023@dce.edu.in>
2026-07-02 20:32:16 +0530

    Time: 1 ms (98.88%) | Memory: 46.5 MB (43.06%) - LeetSync

---
+++ 4058-compute-alternating-sum/compute-alternating-sum.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int alternatingSum(int[] nums) {
+         int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i % 2 == 0)
+                count += nums[i];
+            else
+                count -= nums[i];
+        }
+        return count;
+    
+    }
+}
